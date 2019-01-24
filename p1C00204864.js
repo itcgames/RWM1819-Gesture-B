f0f43b94aa373b500e3088c31a7398134f8e42e4
C00204864 <C00204864@itcarlow.ie>
2019-01-24 10:37:17 +0000

    Added Multi-Touch Deetction

--- p1C00204864.js
+++ p1C00204864.js
@@ -13,27 +13,48 @@ class GestureManager
     this.lastTouchTime = 0;
     this.lastTouchPosX = 0;
     this.lastTouchPosY = 0;
-    this.longTouchThresholdMs = 3000;
+    this.touchCount = 0;
+    this.touches = [];
+
+    // User Defined Callback Thresholds
+    this.longTouchThresholdMs = 2000;
     this.doubleTapThresholdMs = 1000;
+    this.touchCountCallbackThreshold = 2;
+
+    // User Defined Callbacks
     this.doubleTapCallback = function() { /* Empty Function */ };
     this.longTouchCallback = function() { /* Empty Function */ };
     this.touchStartCallbackUser = function(e) { /*console.log(e); console.log("start");*/ };
     this.touchMoveCallbackUser = function(e) { /*console.log(e); console.log("move");*/ };
     this.touchEndCallbackUser = function(e) { /*console.log(e); console.log("end");*/ };
-    document.addEventListener("touchstart", this.touchStartCallback.bind(this), {passive:false});
+    this.swipeCallback = function() { /*console.log(e); console.log("end");*/ };
+    this.pinchCallback = function() { /*console.log(e); console.log("end");*/ };
+    this.touchCountCallback = function() { /*console.log(e); console.log("end");*/ };
 
-    document.addEventListener("touchend", this.touchEndCallback.bind(this), {passive:false})
-    if(this.debug)
-    {
-      document.addEventListener("click", this.touchStartCallback.bind(this), {passive:false});
-    }
-    console.log("Construction Complete");
+    // Main Touch Callbacks
+    document.addEventListener("touchstart", this.touchStartCallback.bind(this), {passive:false});
+    document.addEventListener("touchmove", this.touchMoveCallback.bind(this), {passive: false});
+    document.addEventListener("touchend", this.touchEndCallback.bind(this), {passive:false});
   }
 
   touchStartCallback(e)
   {
     e.preventDefault();
     this.touchStartCallbackUser(e);
+
+    // Manage Recorded Touches
+    this.touches = [];
+    for (var i = 0; i < e.touches.length; i++) 
+    {
+      this.touches.push({x: e.touches[i].clientX, y: e.touches[i].clientY})
+    }
+
+    // Manage Touch Count
+    this.touchCount = e.touches.length;
+    if(this.touchCount > this.touchCountCallbackThreshold) {
+      this.touchCountCallback();
+    }
+
     var touchX = e.touches[0].clientX;
     var touchY = e.touches[0].clientY;
     this.touchDetected = true;
@@ -41,7 +62,7 @@ class GestureManager
     if(touchStartTime < this.lastTouchTime + this.doubleTapThresholdMs)
     {
       this.doubleTapDetetcted = true;
-      if(this.getDistance(this.lastTouchPosX, this.lastTouchPosY, touchX, touchY) < 50)
+      if(this.getDistanceBetweenTouches(this.lastTouchPosX, this.lastTouchPosY, touchX, touchY) < 50)
       {
         if(typeof this.doubleTapCallback === "function")
         {
@@ -62,10 +83,25 @@ class GestureManager
     }
   }
 
+  touchMoveCallback(e) {
+    e.preventDefault();
+    this.touchMoveCallbackUser();
+    this.touches = [];
+    for (var i = 0; i < e.touches.length; i++) 
+    {
+      this.touches.push({x: e.touches[i].clientX, y: e.touches[i].clientY})
+    }
+  }
+
   touchEndCallback(e)
   {
     e.preventDefault();
     this.touchEndCallbackUser(e);
+    this.touches = [];
+    for (var i = 0; i < e.touches.length; i++) 
+    {
+      this.touches.push({x: e.touches[i].clientX, y: e.touches[i].clientY})
+    }
     var touchEndTime = new Date().getTime();
     if(touchEndTime > this.lastTouchTime + this.longTouchThresholdMs)
     {
@@ -78,7 +114,9 @@ class GestureManager
         console.log("DEBUG - Long Touch")
       }
     }
-    this.touchDetected = false;
+    if(e.touches.length === 0) {
+      this.touchDetected = false;
+    }
     this.doubleTapDetected = false;
     if(this.debug)
     {
@@ -86,7 +124,7 @@ class GestureManager
     }
   }
 
-  getDistance(x1, y1, x2, y2)
+  getDistanceBetweenTouches(x1, y1, x2, y2)
   {
     return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
   }
@@ -94,5 +132,13 @@ class GestureManager
   getTouchStatus()
   {
     return this.touchDetected;
+  }
+
+  getTouchCount() {
+    return this.touchCount;
+  }
+
+  getAllTouches() {
+    return this.touches;
   }
 }
